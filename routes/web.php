5834d16e958c3fe3b8ac1818e3a3491e76cbc80e
JosimarPintoCamilo <josimarifmg@gmail.com>
2021-11-12 20:31:11 -0300

    projeto inicial

---
+++ routes/web.php
@@ -0,0 +1,20 @@
+<?php
+
+use App\Jobs\ProcessPodcast;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    ProcessPodcast::dispatch()->onQueue('josimar');
+    return view('welcome');
+});
