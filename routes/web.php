53b37a5c349601190c5795dd89e9152e44f79fac
JosimarPintoCamilo <josimarifmg@gmail.com>
2021-11-12 20:57:07 -0300

    tentativa conf redis

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    ProcessPodcast::dispatch()->onQueue('josimar');
+    ProcessPodcast::dispatch();
     return view('welcome');
 });
